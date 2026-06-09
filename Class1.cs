ff46f296e9edac18caed9cecf8eb9cd24b2965a6
d11116219 <126222904+d11116219@users.noreply.github.com>
2026-06-09 16:19:23 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿public virtual string GetName()
+{
+    return "Person";
+}
+
+public interface IName
+{
+    public string GetName();
+}
+
+public class Student : Person, IName
+{
+    public override string GetName()
+    {
+        return "Student";
+    }
+}
+
+public class Teacher : Person, IName
+{
+    public override string GetName()
+    {
+        return "Teacher";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Class_0609_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person p1;
+            IName p1;
+
+            string str = Console.ReadLine();
+
+            if (str == "1")
+            {
+                p1 = new Student();
+            }
+            else
+            {
+                p1 = new Teacher();
+            }
+
+            Console.WriteLine(p1.GetName());
+        }
+    }
+}
